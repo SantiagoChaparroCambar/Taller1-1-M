95d67e171b72100768c1a0f0c649b332ec617c12
SantiagoChaparroCambar <141367390+SantiagoChaparroCambar@users.noreply.github.com>
2023-08-10 16:02:55 -0500

    Calculadora

---
+++ Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,104 @@
+
+
+package com.mycompany.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+// variables
+         static double num1;
+         static double num2;
+        static double resultado;
+
+///////// Constructor 
+    public Calculadora(double num1, double num2, double resultado) {
+        this.num1= num1;
+        this.num2=num2;
+        this.resultado=resultado;
+    }
+   
+    ////////////////// main 
+    
+    public static void main(String[] args) {
+         Scanner in = new Scanner( System.in);
+         int opc;
+         
+         
+         
+         do{
+             int opc1;
+             System.out.println("\t1. Funciones basicas \n 2.Funciones Trigonometricas "
+                     + "\n 3.Raiz enesima \n 4. potencia enesima \n 5. IVA");
+             System.out.println("Ingrese una opcion ");
+             opc= in.nextInt();
+             switch (opc){
+                 case 1:
+                     System.out.println("\t CALCULADORA " + "\n 1.Suma  \n 2.Resta  "
+                             + "\n 3.multiplicacion \n 4.division ");
+                     opc1=in.nextInt();
+                     
+                     System.out.println("Ingrese dos numeros ");
+                     num1 = in.nextDouble();
+                     num2= in.nextDouble();
+                     
+                     switch(opc1){
+                         case 1:
+                             resultado = num1 + num2;
+                             System.out.println(" suma es :"+ resultado);
+                             break;
+                          case 2:
+                              resultado = num1 - num2;
+                             System.out.println(" resta es :"+ resultado);
+                           break;
+                          case 3:
+                              resultado = num1 * num2;
+                             System.out.println(" multiplicacion es :"+ resultado);
+                              break;
+                          case 4:
+                              resultado = num1 / num2;
+                             System.out.println(" division es :"+ resultado);
+                              break;
+                     }
+                     
+                     break;
+                 case 2:
+                    System.out.println("\t Funciones Trigonometicas"+"\n 1.seno \n 2.coseno \n 3.Tangente"
+                    +" Ingrese una opcion "); 
+                    opc1=in.nextInt();
+                     System.out.println("Ingrese un numero ");
+                     num1 = in.nextDouble();
+                    switch(opc1){
+                        case 1:
+                            resultado= Math.sin(num1);
+                            System.out.println("El seno del numero es "+ resultado);
+                        break;
+                        case 2:
+                            resultado= Math.cos(num1);
+                            System.out.println("El coseno del numero es "+ resultado);
+                        break;
+                        case 3:
+                            resultado= Math.tan(num1);
+                            System.out.println("El tangente del numero es "+ resultado);
+                            break;
+                    }
+                    
+                     break;
+                 case 3:
+                     System.out.println("\t Raiz Enesima \n ingrese un numero");
+                     num1 = in.nextDouble();
+                     break;
+                 case 4:
+                     System.out.println("\t potencia enesima \n ingrese un numero");
+                     num1 = in.nextDouble();
+                     break;
+                 case 5:
+                     System.out.println("\t IVA  \n ingrese un numero");
+                     num1 = in.nextDouble();
+                     break;
+             }
+            
+         }while(opc <5);
+         
+        
+    }
+}
